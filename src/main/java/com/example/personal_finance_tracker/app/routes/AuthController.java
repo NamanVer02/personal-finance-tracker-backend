4f108074805a11311cbf96dc2fa3d8e4c497a985
Naman Verma <namanver.2002@gmail.com>
2025-03-21 09:31:30 +0530

    --

--- src/main/java/com/example/personal_finance_tracker/app/routes/AuthController.java
+++ src/main/java/com/example/personal_finance_tracker/app/routes/AuthController.java
@@ -48,4 +48,6 @@ public class AuthController {
         TokenRefreshResponse response = authService.refreshToken(request.getRefreshToken());
         return ResponseEntity.ok(response);
     }
+
+
 }
