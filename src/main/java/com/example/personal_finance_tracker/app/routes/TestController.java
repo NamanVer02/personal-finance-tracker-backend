7e8fa5291d180a8d7e1c320d92a0fc4847ed4665
Naman Verma <namanver.2002@gmail.com>
2025-04-02 16:30:18 +0530

    --

--- src/main/java/com/example/personal_finance_tracker/app/routes/TestController.java
+++ src/main/java/com/example/personal_finance_tracker/app/routes/TestController.java
@@ -1,13 +1,22 @@
 package com.example.personal_finance_tracker.app.routes;
 
+import com.example.personal_finance_tracker.app.services.DemoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.stream.IntStream;
+
 @RestController
 @RequestMapping("/api/test")
 public class TestController {
+
+    @Autowired
+    private DemoService demoService;
+
     @GetMapping("/all")
     public String allAccess() {
         return "Public Content.";
@@ -29,5 +38,11 @@ public class TestController {
     @PreAuthorize("hasRole('ACCOUNTANT')")
     public String accountantAccess() {
         return "Accountant Board.";
+    }
+
+    @GetMapping("/pooling")
+    public ResponseEntity<String> testConnectionPooling() {
+        IntStream.range(0, 20).parallel().forEach(i -> demoService.performDatabaseOperation(i));
+        return ResponseEntity.ok("Connection pool test completed!");
     }
 }
